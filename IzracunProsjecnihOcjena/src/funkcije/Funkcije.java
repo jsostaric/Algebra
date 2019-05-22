85fdc0427724af95a8d9e89e75269baacf179e80
Jurica Šoštarić <bugificiran@gmail.com>
2019-05-22 14:38:01 +0200

    Add files via upload

--- IzracunProsjecnihOcjena/src/funkcije/Funkcije.java
+++ IzracunProsjecnihOcjena/src/funkcije/Funkcije.java
@@ -1,35 +1,52 @@
 package funkcije;
 
 import java.util.ArrayList;
-
+import studenti.Student;
 
 public class Funkcije {
-	
-	double sumaOcjena;
-	double prosjek;
 
-	
-	//izračunati prosjek ocjena za studente
-	public double prosjekOcjenaStudenata(ArrayList<Integer> ocjene) {
-		
-		for(int i : ocjene) {
-			sumaOcjena += i;
-		}
-		
-		prosjek = sumaOcjena / ocjene.size();
-		
-		
-		
-		return prosjek;
-	}
-	
-	//prosjek ocjena po kolegiju
-	
-	
-	
-	
-	//prosjek ocjena svih studenata
-	
-	
-	
+    public static void izracunajProsjek(ArrayList<Student> listaStudenata) {
+
+        for (int i = 0; i < listaStudenata.size(); i++) {
+
+            int suma = 0;
+            int brojac = 0;
+            double prosjek = 0.0;
+
+            for (int j = 0; j < listaStudenata.get(i).getOcjeneKolegija().size(); j++) {
+                suma += listaStudenata.get(i).getOcjeneKolegija().get(j);
+            }
+
+            prosjek = (double) suma / (double) listaStudenata.get(i).getOcjeneKolegija().size();
+            System.out.println(listaStudenata.get(i).getIme() + " "
+                    + listaStudenata.get(i).getPrezime() + " ("
+                    + listaStudenata.get(i).getMaticniBroj() + ")ima prosjek: " + prosjek);
+        }
+    }
+
+    public static void izracunajProsjekKolegija(String[] kolegij,ArrayList<Student> listaStudenata) {
+
+        ArrayList<String> listaKolegija = new ArrayList<>();
+        
+        ArrayList<Integer> sumaOcjena = new ArrayList<>();
+        for(int i = 0; i < kolegij.length; i++) {
+            listaKolegija.add(kolegij[i]);            
+            sumaOcjena.add(0);
+        }
+        
+        for(int i=0;i<listaStudenata.size(); i++) {
+            ArrayList<String> lk = listaStudenata.get(i).getListaKolegija();
+            ArrayList<Integer> lo = listaStudenata.get(i).getOcjeneKolegija();
+            
+            for(int j=0;j<lk.size(); j++) {
+                int trenutno_stanje = sumaOcjena.get(j);
+                trenutno_stanje += lo.get(j);
+                sumaOcjena.set(j,trenutno_stanje);
+            }
+        }
+        
+        for(int i=0;i<listaKolegija.size(); i++) {            
+                System.out.println("prosjek ocjena za kolegij " + listaKolegija.get(i) + " je: " + (double)sumaOcjena.get(i)/(double)listaStudenata.size());            
+        }        
+    }
 }
--- IzracunProsjecnihOcjena/src/studenti/Student.java
+++ IzracunProsjecnihOcjena/src/studenti/Student.java
@@ -1,97 +1,92 @@
+
 package studenti;
 
 import java.util.ArrayList;
 
+
 public class Student {
+    
+    private static String ustanova;
+    private int godina;
+    private ArrayList<String> listaKolegija;
+    private ArrayList<Integer> ocjeneKolegija;
+    
+    private String ime;
+    private String prezime;
+    private int maticniBroj;
 
-	private String ustanova;
-	private int godina;
-	private ArrayList<String> listaKolegija;
-	private ArrayList<Integer> ocjeneKolegija;
-	
-	private String ime;
-	private String prezime;
-	private int maticniBroj;
-	
-	public Student(String ustanova, int godina, ArrayList<String> listaKolegija, ArrayList<Integer> ocjeneKolegija,
-			String ime, String prezime, int maticniBroj) {
-		
-		this.ustanova = ustanova;
-		this.godina = godina;
-		this.listaKolegija = listaKolegija;
-		this.ocjeneKolegija = ocjeneKolegija;
-		this.ime = ime;
-		this.prezime = prezime;
-		this.maticniBroj = maticniBroj;
-	}
+    public Student(String ustanova, int godina, ArrayList<String> listaKolegija, ArrayList<Integer> ocjeneKolegija, String ime, String prezime, int maticniBroj) {
+        this.ustanova = ustanova;
+        this.godina = godina;
+        this.listaKolegija = listaKolegija;
+        this.ocjeneKolegija = ocjeneKolegija;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.maticniBroj = maticniBroj;
+    }
 
-	public String getUstanova() {
-		return ustanova;
-	}
+    public static String getUstanova() {
+        return ustanova;
+    }
 
-	public void setUstanova(String ustanova) {
-		this.ustanova = ustanova;
-	}
+    public static void setUstanova(String ustanova) {
+        Student.ustanova = ustanova;
+    }
 
-	public int getGodina() {
-		return godina;
-	}
+    public int getGodina() {
+        return godina;
+    }
 
-	public void setGodina(int godina) {
-		this.godina = godina;
-	}
+    public void setGodina(int godina) {
+        this.godina = godina;
+    }
 
-	public ArrayList<String> getListaKolegija() {
-		return listaKolegija;
-	}
+    public ArrayList<String> getListaKolegija() {
+        return listaKolegija;
+    }
 
-	public void setListaKolegija(ArrayList<String> listaKolegija) {
-		this.listaKolegija = listaKolegija;
-	}
+    public void setListaKolegija(ArrayList<String> listaKolegija) {
+        this.listaKolegija = listaKolegija;
+    }
 
-	public ArrayList<Integer> getOcjeneKolegija() {
-		return ocjeneKolegija;
-	}
+    public ArrayList<Integer> getOcjeneKolegija() {
+        return ocjeneKolegija;
+    }
 
-	public void setOcjeneKolegija(ArrayList<Integer> ocjeneKolegija) {
-		this.ocjeneKolegija = ocjeneKolegija;
-	}
+    public void setOcjeneKolegija(ArrayList<Integer> ocjeneKolegija) {
+        this.ocjeneKolegija = ocjeneKolegija;
+    }
 
-	public String getIme() {
-		return ime;
-	}
+    public String getIme() {
+        return ime;
+    }
 
-	public void setIme(String ime) {
-		this.ime = ime;
-	}
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
 
-	public String getPrezime() {
-		return prezime;
-	}
+    public String getPrezime() {
+        return prezime;
+    }
 
-	public void setPrezime(String prezime) {
-		this.prezime = prezime;
-	}
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
 
-	public int getMaticniBroj() {
-		return maticniBroj;
-	}
+    public int getMaticniBroj() {
+        return maticniBroj;
+    }
 
-	public void setMaticniBroj(int maticniBroj) {
-		this.maticniBroj = maticniBroj;
-	}
+    public void setMaticniBroj(int maticniBroj) {
+        this.maticniBroj = maticniBroj;
+    }
 
-	@Override
-	public String toString() {
-		return "Student{" + "godina=" + godina + 
-				", listaKolegija=" + listaKolegija + ", ocjeneKolegija=" + 
-				ocjeneKolegija + ", ime=" + ime + ", prezime=" + prezime + 
-				", maticniBroj=" + maticniBroj + '}';
-	}
-
-	
-	
-	
-	
-	
+    @Override
+    public String toString() {
+        return "Student{" + "godina=" + godina + ", listaKolegija=" + listaKolegija + ", ocjeneKolegija=" + ocjeneKolegija + ", ime=" + ime + ", prezime=" + prezime + ", maticniBroj=" + maticniBroj + '}';
+    }
+        
+    
+    
+    
 }
