4470dd3a46566848139ba7c9297b918d0ed51ff9
Jurica Šoštarić <bugificiran@gmail.com>
2019-06-03 12:50:38 +0200

    Add files via upload

---
+++ Lambda/src/lambda/MyInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambda;
+
+/**
+ *
+ * @author programer10
+ */
+public interface MyInterface {
+    public String print(String text);
+    
+    public static void ispis(String name) {
+        System.out.println(name);
+    }
+}
